304605576ae6dd738538004f5e05a63c678c3f02
Chengzhong <chengzhongpr@gmail.com>
2023-08-31 17:17:38 -0700

    frontend add form rules

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -3,6 +3,38 @@ import styles from "../styles/Form.module.css";
 import { Form, Input, Button, Checkbox } from "antd";
 
 const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const handleSubmit = async (values: {username: string, password: string, remember: boolean}) => {
+    setIsLoading(true);
+    setErrorMessage(null);
+    
+    try {
+      const response = await fetch("FETCH_USER_LOGIN", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: values.username,
+          password: values.password,
+        }),
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        // Handle successful login here, maybe redirecting the user or storing a token
+      } else {
+        setErrorMessage(data.message || "An error occurred during login.");
+      }
+    } catch (error) {
+      setErrorMessage("There was a problem connecting to the server.");
+    }
+  
+    setIsLoading(false);
+  };
+  
   return (
     <div>
       <Form
@@ -10,7 +42,9 @@ const LoginForm = () => {
         initialValues={{
           remember: true,
         }}
+        onFinish={handleSubmit}
       >
+        {errorMessage && <div className={styles.errorMessage}>{errorMessage}</div>}
         <Form.Item
           label="Username"
           name="username"
@@ -19,6 +53,10 @@ const LoginForm = () => {
               required: true,
               message: "Please input your username!",
             },
+            {
+              pattern: /^[a-zA-Z0-9_-]{3,16}$/,
+              message: "Username must be 3-16 characters, and can include numbers, underscores, and hyphens.",
+            }
           ]}
         >
           <Input />
@@ -31,6 +69,10 @@ const LoginForm = () => {
               required: true,
               message: "Please input your password!",
             },
+            {
+              pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+              message: "Password must be 8 characters long and contain one uppercase, one lowercase, one number, and one special character.",
+            }
           ]}
         >
           <Input.Password />
@@ -39,9 +81,9 @@ const LoginForm = () => {
           <Checkbox>Remember me</Checkbox>
         </Form.Item>
         <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Login
-          </Button>
+        <Button type="primary" htmlType="submit" loading={isLoading} disabled={isLoading}>
+          Login
+        </Button>
         </Form.Item>
       </Form>
     </div>
--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -1,10 +1,56 @@
 import { Form, Input, Button } from "antd";
+import React, { useState } from "react";
+
 import styles from "../styles/Form.module.css";
 
 const RegisterForm: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const handleRegister = async (values: {
+    username: string;
+    email: string;
+    password: string;
+  }) => {
+    setIsLoading(true);
+    setErrorMessage(null);
+
+    try {
+      const response = await fetch(
+        "YOUR_BACKEND_API_ENDPOINT_FOR_REGISTRATION",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: values.username,
+            email: values.email,
+            password: values.password,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+      } else {
+        setErrorMessage(
+          data.message || "An error occurred during registration."
+        );
+      }
+    } catch (error) {
+      setErrorMessage("There was a problem connecting to the server.");
+    }
+
+    setIsLoading(false);
+  };
+
   return (
     <div>
-      <Form className={styles.loginForm}>
+      <Form className={styles.loginForm} onFinish={handleRegister}>
+        {errorMessage && (
+          <div className={styles.errorMessage}>{errorMessage}</div>
+        )}
         <Form.Item
           label="Username"
           name="username"
@@ -13,6 +59,10 @@ const RegisterForm: React.FC = () => {
               required: true,
               message: "Please input your username!",
             },
+            {
+              pattern: /^[a-zA-Z0-9_-]{3,16}$/,
+              message: "Username must be 3-16 characters, and can include numbers, underscores, and hyphens.",
+            }
           ]}
         >
           <Input placeholder="Choose a username" />
@@ -25,6 +75,10 @@ const RegisterForm: React.FC = () => {
               required: true,
               message: "Please input your email!",
             },
+            {
+              type: 'email',
+              message: 'The input is not a valid email!',
+            }
           ]}
         >
           <Input placeholder="Enter your email" />
@@ -37,12 +91,21 @@ const RegisterForm: React.FC = () => {
               required: true,
               message: "Please input your password!",
             },
+            {
+              pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+              message: "Password must be 8 characters long and contain one uppercase, one lowercase, one number, and one special character.",
+            }
           ]}
         >
           <Input.Password placeholder="Choose a password" />
         </Form.Item>
         <Form.Item>
-          <Button type="primary" htmlType="submit">
+          <Button
+            type="primary"
+            htmlType="submit"
+            loading={isLoading}
+            disabled={isLoading}
+          >
             Register
           </Button>
         </Form.Item>
