bf5583efc025364da099ee785dc0a24d56361c80
Chenglin Wei <chengliw@andrew.cmu.edu>
2023-08-31 17:47:35 -0700

    Fix login and register logic

--- backend/app/main/routes.py
+++ backend/app/main/routes.py
@@ -8,34 +8,40 @@ def home():
 
 @main.route('/register', methods=['POST'])
 def register():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-    email = data.get('email')
-    if(username is None or password is None or email is None):
-        return jsonify({'message': 'Missing required fields'}), 400
-    
-    user = User(name=username, email=email, password=password)
     try:
-        user.save()
-    except Exception as e:
-        app.logger.error(e)
-        return jsonify({'message': 'User already exists'}), 409
-    
-    return jsonify({'message': 'User registered'})
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        email = data.get('email')
+        if(username is None or password is None or email is None):
+            return jsonify({'message': 'Missing required fields'}), 400
+        
+        user = User(name=username, email=email, password=password)
+        try:
+            user.save()
+        except Exception as e:
+            app.logger.error(e)
+            return jsonify({'message': 'User already exists'}), 409
+        
+        return jsonify({'message': 'User registered'})
+    except:
+        return jsonify({'message': 'Registration failed'}), 500
 
 @main.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
+    try:
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
 
-    user = User.objects(name=username).first()
-    if user is None:
-        return jsonify({'message': 'User not found'}), 404
-    if user.password != password:
-        return jsonify({'message': 'Incorrect password'}), 401
-    return jsonify({'message': 'User logged in'})
+        user = User.objects(name=username).first()
+        if user is None:
+            return jsonify({'message': 'User not found'}), 404
+        if user.password != password:
+            return jsonify({'message': 'Incorrect password'}), 401
+        return jsonify({'message': 'User logged in'})
+    except:
+        return jsonify({'message': 'Login failed'}), 500
     
 @main.route('/subscribe', methods=['POST'])
 def subscribe():
